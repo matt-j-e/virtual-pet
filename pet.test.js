c3edae08f7f65521d11b6f2eec45955ab210ce93
mattedwards <the.matt.edwards@googlemail.com>
2021-01-13 20:45:23 +0000

    the pet now experiences hunger and gets less fit as it gets older

--- pet.test.js
+++ pet.test.js
@@ -2,7 +2,7 @@ const {
     Pet
 } = require("./pet.js");
 
-describe("Runs tests on a JavaScript virtual pet", () => {
+describe("Checks on object setup", () => {
 
     it("checks that an instance of an object is created", () => {
         expect(new Pet('Fido')).toBeInstanceOf(Pet);
@@ -18,6 +18,16 @@ describe("Runs tests on a JavaScript virtual pet", () => {
         expect(fido.age).toBe(0);
     });
 
+    it("has initial hunger of 0", () => {
+        const fido = new Pet('Fido');
+        expect(fido.hunger).toBe(0);
+    });
+
+    it("has initial fitness of 10", () => {
+        const fido = new Pet('Fido');
+        expect(fido.fitness).toBe(10);
+    })
+
 });
 
 describe("growUp", () => {
@@ -28,4 +38,17 @@ describe("growUp", () => {
         expect(fido.age).toBe(1);
     });
 
+    it("increments hunger by 5", () => {
+        const fido = new Pet("Fido");
+        fido.growUp();
+        expect(fido.hunger).toBe(5);
+    });
+
+    it("decrements hungefitness by 3", () => {
+        const fido = new Pet("Fido");
+        fido.growUp();
+        expect(fido.fitness).toBe(10-3);
+    });
+
 });
+
